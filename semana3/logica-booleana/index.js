a04131df67acb0a80b40569f7a316208ca01e2fc
rtakeshi13 <rtakeshi13@gmail.com>
2020-04-27 13:23:24 -0300

    interpretacao - ex2

--- semana3/logica-booleana/index.js
+++ semana3/logica-booleana/index.js
@@ -20,3 +20,37 @@ console.log("d. ", resultado)
 console.log("e. ", typeof resultado)
 //e. boolean
 
+//Exercício 2
+//a. O array é um conjunto com varias unidades de informação; o array se declara colocando os valores entre [] e separados por , (vírgula)
+//b. O index inicial é 0
+//c. Usando o comando .lenght (ex: tamanho = array.lenght)
+//d.
+console.log("Exercicio 2")
+let array
+console.log('I. ', array)
+//I. undefined
+array = null
+console.log('II. ', array)
+//II. null
+
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('III. ', array.length)
+//III. 11
+let i = 0
+console.log('IV. ', array[i], " e ", array[i+1])
+//IV. 3 e 4
+array[i+1] = 19
+const valor = array[i+6]
+console.log('V. ', array[i+1], " e ", valor)
+//V. 19 e 9
+i+=1
+array[i] = array[i-1]
+console.log('VI. ', array[i])
+//VI. 3
+i = array.length - 1
+array[i] = array[i-3]
+const resultadoC = array[i]%array[1]
+console.log('VII. ', resultadoC)
+//VII. 1
+
+
