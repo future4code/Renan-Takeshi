d83e4efde57fa3123c4b80712ad4ed99fd4b6188
rtakeshi13 <rtakeshi13@gmail.com>
2020-04-28 11:41:21 -0300

    exercicio 2

--- semana3/condicionais/exercicio2/index.js
+++ semana3/condicionais/exercicio2/index.js
@@ -1,11 +1,31 @@
-let resultado1 = 3 + 4;
-console.log(resultado1)
+// Atribui a string digitada pelo usuario para a variavel fruta
+let fruta = prompt("Escolha uma fruta")
+// Desclaração da variavel preco para ser alterada no switch
+let preco
+// Atribui um valor para preco dependendo de qual fruta foi digitada pelo usuario
+switch (fruta) {
+  case "Laranja":
+    preco = 3.5
+    break;
+  case "Maçã":
+    preco = 2.25
+    break;
+  case "Uva":
+    preco = 0.30
+    break;
+  case "Pêra":
+    preco = 5.5
+    break; // BREAK PARA O ITEM d.
+  default:
+    preco = 5
+    break;
+}
+// Imprime no console a fruta escolhida e seu valor
+console.log("O preço da fruta ", fruta, " é ", "R$ ", preco)
 
-let resultado2 = 3 * 5 / 2
-console.log(resultado2)
+//b. O preço da fruta Maçã é R$ 2.25
 
-let resultado3 = (4 - 5) * (-1)
-console.log(resultado3)
+//c. (2 * 3.5) + (1 * 2.25) + (3 * 5) + (1 * 0.30) = 24.55
+//c. Pagaria R$ 24.55, pois Banana não tem case e vai pro default
 
-let resultado4 = 234 % 5;
-console.log(resultado4)
+//d. O preço da fruta Pêra é R$ 5
