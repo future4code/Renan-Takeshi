be342584c4246c7faa2ab8f48304b7afb60ede71
rtakeshi13 <rtakeshi13@gmail.com>
2020-04-28 23:38:00 -0300

    desafio - enxutada no codigo

--- semana3/condicionais/desafio/index.js
+++ semana3/condicionais/desafio/index.js
@@ -4,11 +4,13 @@ const tipo = prompt("Qual tipo de jogo gostaria ? Responda IN ou DO").toUpperCas
 const etapa = prompt("Qual a etapa do jogo ? Responda SF, DT ou FI").toUpperCase()
 const categoria = Number(prompt("Escolha a categoria: 1, 2, 3 ou 4"))
 const quantidade = Number(prompt("Quantos ingressos deseja comprar ?"))
-
 // Tabela de preços
 const sf = [1320, 880, 550, 220]
 const dt = [660, 440, 330, 170]
 const fi = [1980, 1320, 880, 330]
+// Variaveis
+let valorIngresso
+let valorTotal
 
 // Saida
 console.log("---Dados da compra---")
@@ -23,16 +25,19 @@ switch(tipo){
         console.log("Tipo de jogo: Nacional")
         break;
 }
-// Etapa do jogo
+// Etapa do jogo e valores do ingresso
 switch(etapa){
     case "SF":
         console.log("Etapa do jogo: Semi-final")
+        valorIngresso = sf[categoria-1]
         break;
     case "DT":
         console.log("Etapa do jogo: Decisão do 3o lugar")
+        valorIngresso = dt[categoria-1]
         break;
     case "FI":
         console.log("Etapa do jogo: Final")
+        valorIngresso = fi[categoria-1]
         break;
 }
 // Categoria
@@ -40,19 +45,8 @@ console.log(`Categoria: ${categoria}`)
 // Quantidade
 console.log(`Quantidade de ingressos: ${quantidade}`)
 console.log("---Valores---")
-// Valores do ingresso
-let valorIngresso
-if(etapa === "SF"){
-    valorIngresso = sf[categoria-1]
-}else
-if(etapa === "DT"){
-    valorIngresso = dt[categoria-1]
-}else
-if(etapa === "FI"){
-    valorIngresso = fi[categoria-1]
-}
 // Valor total
-let valorTotal = valorIngresso * quantidade
+valorTotal = valorIngresso * quantidade
 if(tipo === "IN"){
     valorIngresso = valorIngresso * 4.1
     valorTotal = valorTotal * 4.1
