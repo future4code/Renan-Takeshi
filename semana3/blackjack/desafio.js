edf8f3de18362dc8af30e15b4a8f2fb22f3acc90
rtakeshi13 <rtakeshi13@gmail.com>
2020-04-30 13:04:37 -0300

    blackjack desafio

--- semana3/blackjack/desafio.js
+++ semana3/blackjack/desafio.js
@@ -11,3 +11,63 @@
  * 
  */
 
+// Mensagem de boas vindas
+console.log('Bem vindo ao jogo de Blackjack! - Desafio')
+// Loop principal do jogo
+while(confirm('Quer iniciar uma nova rodada ? - Desafio')){
+   // Iniciando variaveis
+   // Aqui faz mais sentido iniciar dentro do loop!
+   let cartasUsuario = []
+   let cartasComputador = []
+   let pontuacaoUsuario = 0
+   let pontuacaoComputador = 0
+   let novaCompra = null
+   // Comprando cartas iniciais
+   // Tô me sentindo meio trapaceiro por usar do-while =/
+   do{
+      for(let i =0; i < 2; i++){
+         novaCompra = comprarCarta()
+         cartasUsuario.push(novaCompra.texto)
+         pontuacaoUsuario += novaCompra.valor
+      }
+      for(let i =0; i < 2; i++){
+         novaCompra = comprarCarta()
+         cartasComputador.push(novaCompra.texto)
+         pontuacaoComputador += novaCompra.valor
+      }  
+   // A pontuação pra dois A é 22
+   }while(pontuacaoUsuario === 22 || pontuacaoComputador === 22)
+   alert(`Suas cartas são ${cartasUsuario}. A carta revelada do computador é ${cartasComputador[0]}`)
+   
+   // Usuario comprando cartas
+   while(confirm('Deseja comprar mais uma carta ?')){
+      novaCompra = comprarCarta()
+      cartasUsuario.push(novaCompra.texto)
+      pontuacaoUsuario += novaCompra.valor
+      alert(`Suas cartas são ${cartasUsuario}. A carta revelada do computador é ${cartasComputador[0]}`)
+   }
+   // Pontuacao do usuario extrapolou 21
+   if(pontuacaoUsuario > 21){
+      alert(`Suas cartas são ${cartasUsuario}. Sua pontuação é ${pontuacaoUsuario}\nO usuário perdeu pois ultrapassou 21!`)
+   }else{
+      // Loop do computador comprando cartas
+      while(pontuacaoComputador < pontuacaoUsuario){
+         novaCompra = comprarCarta()
+         cartasComputador.push(novaCompra.texto)
+         pontuacaoComputador += novaCompra.valor
+      }
+      if(pontuacaoComputador > 21){
+         alert(`Suas cartas são ${cartasUsuario}. Sua pontuação é ${pontuacaoUsuario}.\nAs cartas do computador são ${cartasComputador}. A pontuação do computador é ${pontuacaoComputador}.\nO usuário ganhou!`)
+      }else
+      if(pontuacaoUsuario > pontuacaoComputador){
+         alert(`Suas cartas são ${cartasUsuario}. Sua pontuação é ${pontuacaoUsuario}.\nAs cartas do computador são ${cartasComputador}. A pontuação do computador é ${pontuacaoComputador}.\nO usuário ganhou!`)
+      }else
+      if(pontuacaoComputador > pontuacaoUsuario){
+         alert(`Suas cartas são ${cartasUsuario}. Sua pontuação é ${pontuacaoUsuario}.\nAs cartas do computador são ${cartasComputador}. A pontuação do computador é ${pontuacaoComputador}.\nO computador ganhou ganhou!`)
+      }else{
+         alert(`Suas cartas são ${cartasUsuario}. Sua pontuação é ${pontuacaoUsuario}.\nAs cartas do computador são ${cartasComputador}. A pontuação do computador é ${pontuacaoComputador}.\nEmpate!`)
+      }
+   }
+}
+// Término do jogo
+console.log('O jogo acabou')
