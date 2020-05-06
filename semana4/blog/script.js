2eac49f3b988dfeba0307b0c5d527d24d8d5947b
rtakeshi13 <rtakeshi13@gmail.com>
2020-05-06 15:39:26 -0300

    falta o desafio de mudar de pagina

--- semana4/blog/script.js
+++ semana4/blog/script.js
@@ -1,7 +1,8 @@
 const blogPost = {
     titulo: null,
     autor: null,
-    texto: null
+    texto: null,
+    imagem: null
 }
 
 let arrPosts = []
@@ -10,20 +11,38 @@ function enviarPost(){
     let postTitulo = document.getElementById('titulo')
     let postAutor = document.getElementById('autor')
     let postTexto = document.getElementById('texto')
+    let postImagem = document.getElementById('imagem')
     let novoPost = {
         ...blogPost,
         titulo: postTitulo.value,
         autor: postAutor.value,
-        texto: postTexto.value
+        texto: postTexto.value,
     }
-    document.getElementById('posts').innerHTML +=  `<article>
+    if(validarImagem(postImagem.value)){
+        document.getElementById('posts').innerHTML +=  `<article>
+                                                        <h1>${postTitulo.value}</h1>
+                                                        <h2><i>autor: ${postAutor.value}</i></h2>
+                                                        <img src="${postImagem.value}" alt="">
+                                                        <p>${postTexto.value.replace(/\n/g,'<br>')}</p>
+                                                        </article>`
+        novoPost.imagem = postImagem.value
+    } else
+    {
+        document.getElementById('posts').innerHTML +=  `<article>
                                                         <h1>${postTitulo.value}</h1>
                                                         <h2><i>autor: ${postAutor.value}</i></h2>
                                                         <p>${postTexto.value.replace(/\n/g,'<br>')}</p>
-                                                    </article>`
+                                                        </article>`
+    }
     postTitulo.value = ''
     postAutor.value = ''
     postTexto.value = ''
+    postImagem.value = ''
     arrPosts.push(novoPost)
-    console.log(arrPosts)
+    console.log(arrPosts)    
+}
+
+function validarImagem(texto){
+    let str = texto.slice(-4,texto.length)
+    return str.match(/\.(jpeg|jpg|gif|png)$/) 
 }
