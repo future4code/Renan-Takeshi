90a719442dbdcac5b470a8a2facae113b4d37542
rtakeshi13 <rtakeshi13@gmail.com>
2020-05-08 14:38:54 -0300

    funcoes de array 5

--- semana4/lista-de-exercicios/script.js
+++ semana4/lista-de-exercicios/script.js
@@ -266,3 +266,19 @@ function criarEmails(arr){
 }
 
 // 5
+const contas = [
+	{ cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
+	{ cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
+	{ cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
+	{ cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
+	{ cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
+	{ cliente: "Soter", saldoTotal: 1200, compras: [] }
+]
+
+function atualizaSalto(arr){
+    arr.forEach((conta) => {
+        for(let compra of conta.compras){
+            conta.saldoTotal -= compra
+        }
+    })
+}
