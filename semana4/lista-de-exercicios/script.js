d1bbe979b922f4a784c73f319f3a1f826749733d
rtakeshi13 <rtakeshi13@gmail.com>
2020-05-08 10:23:54 -0300

    interpretacao 1

---
+++ semana4/lista-de-exercicios/script.js
@@ -0,0 +1,8 @@
+// Exercícios de interpretação
+
+// 1.
+// A função conversorDeMoeda recebe um argumento number
+// e converte esse valor para moeda Real utilizando uma cotação
+// informada pelo usuário através de um prompt, retornando uma string.
+// Supondo que o usuário responda o prompt com o valor 5, a resposta no console seria:
+// R$500
