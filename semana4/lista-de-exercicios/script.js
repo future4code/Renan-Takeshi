aca07cf1c2fa6bfc791eac7c9dd87ced0c70d339
rtakeshi13 <rtakeshi13@gmail.com>
2020-05-08 10:31:22 -0300

    interpretacao 2

--- semana4/lista-de-exercicios/script.js
+++ semana4/lista-de-exercicios/script.js
@@ -6,3 +6,14 @@
 // informada pelo usuário através de um prompt, retornando uma string.
 // Supondo que o usuário responda o prompt com o valor 5, a resposta no console seria:
 // R$500
+
+// 2.
+// A função investeDinheiro recebe um Number valor que sera
+// multiplicado por uma taxa determinada a ser selecionada pela
+// string tipoDeInvestimento, que é o primeiro parâmetro da função
+// Caso o tipoDeInvestimento seja inválido, mostra um alert
+// retorna um Number com o valor multiplicado pela taxa
+// Respostas do console, em ordem:
+// 161
+// undefined (com um alert "TIPO DE INVESTIMENTO INFORMADO INCORRETO!")
+
