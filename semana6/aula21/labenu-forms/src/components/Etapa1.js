2fc20e4a900d6a518a5f5b54795ae84ac1811223
rtakeshi13 <rtakeshi13@gmail.com>
2020-05-18 22:20:59 -0300

    desafio 4

--- semana6/aula21/labenu-forms/src/components/Etapa1.js
+++ semana6/aula21/labenu-forms/src/components/Etapa1.js
@@ -9,18 +9,52 @@ export const Container = styled.div`
   flex-direction: column;
   align-items:center;
 `
+
 export const Botao = styled.button`
   margin-top: 20px;
 `
 
 class Etapa1 extends React.Component {
+  state = {
+    nome: "",
+    nomeErro: "",
+    idade: "",
+    idadeErro: "",
+    email: "",
+    emailErro: "",
+    erro: true,
+  }
+
+  validarEtapa = () => {
+    if (!this.validarTexto(this.state.email)) {
+      this.setState({ emailErro: "Preencha seu email", erro: true })
+    } else {
+      this.setState({ emailErro: "", erro: false })
+    }
+  }
+
+  prosseguir = () => {
+    this.validarEtapa();
+    setTimeout(() => {
+      if (this.state.erro) {
+        alert("Você deve preencher seu email antes de continuar")
+      } else {
+        this.props.avancarEtapa();
+      }
+    }, 1)
+  }
+
+  setNome = (nome) => { this.setState({ nome: nome }) }
+  setIdade = (idade) => { this.setState({ idade: idade }) }
+  setEmail = (email) => { this.setState({ email: email }) }
+
   render() {
     return (
       <Container>
         <h3>ETAPA 1 - DADOS GERAIS</h3>
-        <PerguntaAberta pergunta={"1. Qual o seu nome?"} />
-        <PerguntaAberta pergunta={"2. Qual sua idade?"} />
-        <PerguntaAberta pergunta={"3. Qual seu email?"} />
+        <PerguntaAberta setValue={this.setNome} msgErro={this.state.nomeErro} pergunta={"1. Qual o seu nome?"} />
+        <PerguntaAberta setValue={this.setIdade} msgErro={this.state.idadeErro} pergunta={"2. Qual sua idade?"} />
+        <PerguntaAberta setValue={this.setEmail} msgErro={this.state.emailErro} pergunta={"3. Qual seu email? (OBRIGATÓRIO)"} />
         <PerguntaOpcoes
           vaiEtapaDois={this.props.vaiEtapaDois}
           vaiEtapaTres={this.props.vaiEtapaTres}
@@ -32,10 +66,18 @@ class Etapa1 extends React.Component {
             "Ensino superior completo"
           ]}
         />
-        <Botao onClick={() => { this.props.avancarEtapa() }}>Continuar</Botao>
+        <Botao onClick={this.prosseguir}>Prosseguir</Botao>
       </Container>
     );
   }
+
+  validarTexto = (texto) => {
+    if (texto.replace(/ /g, "").length !== 0) {
+      return true
+    }
+    return false
+  }
+
 }
 
 export default Etapa1;
--- semana6/aula21/labenu-forms/src/components/PerguntaAberta.js
+++ semana6/aula21/labenu-forms/src/components/PerguntaAberta.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Container } from './Etapa1'
+import styled from 'styled-components';
 
+const Erro = styled.p`
+    color: red;
+    margin-bottom: 0px;
+`
 
 class PerguntaAberta extends React.Component {
     state = {
@@ -8,12 +13,14 @@ class PerguntaAberta extends React.Component {
     }
 
     onChangeInput = (event) => {
+        this.props.setValue(event.target.value)
         this.setState({ valorInput: event.target.value });
     };
 
     render() {
         return (
             <Container>
+                <Erro>{this.props.msgErro}</Erro>
                 <p>{this.props.pergunta}</p>
                 <input value={this.state.valorInput} onChange={this.onChangeInput} />
             </Container>
