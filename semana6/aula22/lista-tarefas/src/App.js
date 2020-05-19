7e155cd9cd74333aeb2c53fa6751436de1edf75c
rtakeshi13 <rtakeshi13@gmail.com>
2020-05-19 11:24:23 -0300

    npm install

---
+++ semana6/aula22/lista-tarefas/src/App.js
@@ -0,0 +1,99 @@
+import React from 'react'
+import styled from 'styled-components'
+import './styles.css'
+
+const TarefaList = styled.ul`
+  padding: 0;
+  width: 200px;
+`
+
+const Tarefa = styled.li`
+  text-align: left;
+  text-decoration: ${({completa}) => (completa ? 'line-through' : 'none')};
+`
+
+const InputsContainer = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  gap: 10px;
+`
+
+class App extends React.Component {
+    state = {
+      tarefas: [],
+      inputValue: '',
+      filter: ''
+    }
+
+  componentDidUpdate() {
+
+  };
+
+  componentDidMount() {
+
+  };
+
+  onChangeInput = (event) => {
+
+  }
+
+  criaTarefa = () => {
+
+  }
+
+  selectTarefa = (id) => {
+
+  }
+
+  onChangeFilter = (event) => {
+
+  }
+
+  render() {
+    const listaFiltrada = this.state.tarefas
+      .filter(tarefa => {
+        switch (this.state.filter) {
+          case 'pendentes':
+            return !tarefa.completa
+          case 'completas':
+            return tarefa.completa
+          default:
+            return true
+        }
+      })
+
+    return (
+      <div className="App">
+        <h1>Lista de tarefas</h1>
+        <InputsContainer>
+          <input value={this.state.inputValue} onChange={this.onChangeInput}/>
+          <button onClick={this.criaTarefa}>Adicionar</button>
+        </InputsContainer>
+        <br/>
+
+        <InputsContainer>
+          <label>Filtro</label>
+          <select value={this.state.filter} onChange={this.onChangeFilter}>
+            <option value="">Nenhum</option>
+            <option value="pendentes">Pendentes</option>
+            <option value="completas">Completas</option>
+          </select>
+        </InputsContainer>
+        <TarefaList>
+          {listaFiltrada.map(tarefa => {
+            return (
+              <Tarefa
+                completa={tarefa.completa}
+                onClick={() => this.selectTarefa(tarefa.id)}
+              >
+                {tarefa.texto}
+              </Tarefa>
+            )
+          })}
+        </TarefaList>
+      </div>
+    )
+  }
+}
+
+export default App
