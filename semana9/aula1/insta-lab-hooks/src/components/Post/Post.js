403da2c4526231b34f70b3ca9d75489e0c9f53e5
Renan Takeshi <rtakeshi13@gmail.com>
2020-06-15 15:34:18 -0300

    entrega obrigatoria

---
+++ semana9/aula1/insta-lab-hooks/src/components/Post/Post.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import {
+  PostContainer,
+  PostHeader,
+  UserPhoto,
+  PostPhoto,
+  PostFooter,
+  CommentContainer,
+} from "./styles";
+
+import IconeComContador from "../IconeComContador/IconeComContador";
+import SecaoComentario from "../SecaoComentario/SecaoComentario";
+
+import iconeCoracaoBranco from "../../img/favorite-white.svg";
+import iconeCoracaoPreto from "../../img/favorite.svg";
+import iconeComentario from "../../img/comment_icon.svg";
+
+const Post = (props) => {
+  const [curtido, setCurtido] = useState(false);
+  const [numeroCurtidas, setNumeroCurtidas] = useState(0);
+  const [comentando, setComentando] = useState(false);
+  const [numeroComentarios, setNumeroComentarios] = useState(0);
+  const [comentarios, setComentarios] = useState([]);
+
+  const onClickCurtida = () => {
+    if (curtido) {
+      setCurtido(!curtido);
+      setNumeroCurtidas(numeroCurtidas - 1);
+    } else {
+      setCurtido(!curtido);
+      setNumeroCurtidas(numeroCurtidas + 1);
+    }
+  };
+
+  const onClickComentario = () => {
+    setComentando(!comentando);
+  };
+
+  const enviarComentario = (comentario) => {
+    const listaDeComentarios = [...comentarios, comentario]
+    setComentarios(listaDeComentarios)
+    setComentando(false)
+    setNumeroComentarios(numeroComentarios+1)
+  };
+
+  const iconeCurtida = curtido ? (iconeCoracaoPreto) : (iconeCoracaoBranco)
+
+  const caixaDeComentario = comentando ? (
+    <SecaoComentario enviarComentario={enviarComentario}/>
+  ) : (
+    comentarios.map(comentario => {
+      return (
+        <CommentContainer>
+          <p>{comentario}</p>
+        </CommentContainer>
+      )
+    })
+  )
+
+  return (
+    <PostContainer>
+      <PostHeader>
+        <UserPhoto src={props.fotoUsuario} alt={"Imagem do usuario"} />
+        <p>{props.nomeUsuario}</p>
+      </PostHeader>
+
+      <PostPhoto src={props.fotoPost} alt={"Imagem do post"} />
+
+      <PostFooter>
+        <IconeComContador
+          icone={iconeCurtida}
+          onClickIcone={onClickCurtida}
+          valorContador={numeroCurtidas}
+        />
+
+        <IconeComContador
+          icone={iconeComentario}
+          onClickIcone={onClickComentario}
+          valorContador={numeroComentarios}
+        />
+      </PostFooter>
+      {caixaDeComentario}
+    </PostContainer>
+  );
+};
+
+export default Post;
