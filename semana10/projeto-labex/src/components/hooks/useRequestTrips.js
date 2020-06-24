9119f5aec10492e70f00e4536554a553b29ab3ab
Renan Takeshi <rtakeshi13@gmail.com>
2020-06-24 13:39:46 -0300

    custom hooks

---
+++ semana10/projeto-labex/src/components/hooks/useRequestTrips.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+import * as labex from "../functions/axios";
+
+const useRequestTrips = () => {
+  const [trips, setTrips] = useState();
+
+  useEffect(() => {
+    async function getTrips() {
+      const response = await labex.getTrips();
+      setTrips(response);
+    }
+    getTrips();
+  }, []);
+
+  return trips;
+};
+export default useRequestTrips;
