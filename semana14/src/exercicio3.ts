f89942e9b52b0c079a4f46c46a675e1db692dce8
Renan Takeshi <rtakeshi13@gmail.com>
2020-07-27 12:22:02 -0300

    exercicio3

---
+++ semana14/src/exercicio3.ts
@@ -0,0 +1,33 @@
+type post = {
+  autor: string;
+  texto: string;
+};
+
+const posts: post[] = [
+  {
+    autor: "Alvo Dumbledore",
+    texto: "Não vale a pena viver sonhando e se esquecer de viver",
+  },
+  {
+    autor: "Severo Snape",
+    texto: "Menos 10 pontos para Grifinória!",
+  },
+  {
+    autor: "Hermione Granger",
+    texto: "É levi-ô-sa, não levio-sá!",
+  },
+  {
+    autor: "Dobby",
+    texto: "Dobby é um elfo livre!",
+  },
+  {
+    autor: "Lord Voldemort",
+    texto: "Avada Kedavra!",
+  },
+];
+
+const buscarPostsPorAutor = (posts: post[], autorInformado: string): post[] => {
+  return posts.filter((post: post) => {
+    return post.autor === autorInformado;
+  });
+};
