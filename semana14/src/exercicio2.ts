f3526264ccc232b2739b76b6c73ff0bb6392defd
Renan Takeshi <rtakeshi13@gmail.com>
2020-07-27 12:17:58 -0300

    exercicio2

---
+++ semana14/src/exercicio2.ts
@@ -0,0 +1,22 @@
+function obterEstatisticas(numeros: number[]) {
+  const numerosOrdenados: number[] = numeros.sort((a, b) => a - b);
+
+  let soma: number = 0;
+
+  for (let num of numeros) {
+    soma += num;
+  }
+
+  const estatisticas: object = {
+    maior: numerosOrdenados[numeros.length - 1],
+    menor: numerosOrdenados[0],
+    media: soma / numeros.length,
+  };
+
+  return estatisticas;
+}
+
+type amostra = {
+  numeros: [];
+  obterEstatisticas: (numeros: number[]) => object;
+};
