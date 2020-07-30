41485e7d5d1463e239af8c9958408fe75d2fd2fb
Renan Takeshi <rtakeshi13@gmail.com>
2020-07-30 12:07:42 -0300

    ex1 ex2

--- semana14/aula4/src/index.ts
+++ semana14/aula4/src/index.ts
@@ -1,1 +1,36 @@
-console.log("oi");
+import axios from "axios";
+
+const baseUrl = "https://us-central1-labenu-apis.cloudfunctions.net/labenews/";
+
+// exercicio 1
+// item A)
+// labenews/subscribers/all
+
+// item B)
+// Promise<any[]>
+
+// item C)
+async function getSubscribers(): Promise<any[]> {
+  const res = await axios.get(baseUrl + "subscribers/all");
+  return res.data;
+}
+
+// main
+(async () => {
+  console.log(await getSubscribers());
+})();
+
+// exercicio 2
+// item A)
+// Uma arrow function eh declarada como:
+// const nomeDaFuncao = async (<parametros>): <type do return> => <corpo da funcao>
+// Um funcao nomeda eh declarada como:
+// async function nomeDaFuncao (<parametros>): <type do return> {<corpo da funcao>}
+
+// item B)
+const arrowGetSubscribers = async (): Promise<any[]> => {
+  const res = await axios.get(baseUrl + "subscribers/all");
+  return res.data;
+};
+
+// exercicio 3
