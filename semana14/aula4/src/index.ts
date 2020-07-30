71fe25b20063bb789e4e72c8a1f30579f8158603
Renan Takeshi <rtakeshi13@gmail.com>
2020-07-30 13:48:55 -0300

    exercicios obrigatorios

--- semana14/aula4/src/index.ts
+++ semana14/aula4/src/index.ts
@@ -1,4 +1,7 @@
 import axios from "axios";
+import * as moment from "moment";
+
+moment.locale("pt-br");
 
 const baseUrl = "https://us-central1-labenu-apis.cloudfunctions.net/labenews/";
 
@@ -15,11 +18,6 @@ async function getSubscribers(): Promise<any[]> {
   return res.data;
 }
 
-// main
-(async () => {
-  console.log(await getSubscribers());
-})();
-
 // exercicio 2
 // item A)
 // Uma arrow function eh declarada como:
@@ -34,3 +32,118 @@ const arrowGetSubscribers = async (): Promise<any[]> => {
 };
 
 // exercicio 3
+type User = {
+  id: string;
+  name: string;
+  email: string;
+};
+
+// item A)
+// Nao recebi erro de tipagem
+
+// item B)
+//
+
+// item C)
+const arrowGetSubscribersTypeUser = async (): Promise<User[]> => {
+  const res = await axios.get(baseUrl + "subscribers/all");
+  return res.data;
+};
+
+// exercicio 4
+// item A)
+// void, pois nao retorna nada e nao recebe resposta
+
+// item B)
+const createNewItem = (title: string, content: string): void => {
+  axios.put(baseUrl + "news", { title, content, date: Date.now() });
+};
+
+// exercicio 5
+// item A)
+
+// item B)
+const sendNotifications = (users: User[], message: string): void => {
+  for (let user of users) {
+    axios.post(baseUrl + "notifications/send", {
+      subscriberId: user.id,
+      message,
+    });
+  }
+};
+
+// exercicio 6
+// item A)
+// Promise.all returna um um array com o resultado das promises passadas como argumento
+
+// item B)
+const sendNotificationsWithPromiseAll = async (
+  users: any[],
+  message: string
+): Promise<void> => {
+  const promisesArray: any[] = [];
+
+  for (let user of users) {
+    promisesArray.push(
+      axios.post(baseUrl + "notifications/send", {
+        subscriberId: user.id,
+        message,
+      })
+    );
+  }
+
+  await Promise.all(promisesArray);
+};
+
+// exercicio 7
+// item A)
+const signup = (name: string, email: string): void => {
+  axios.put(baseUrl + "subscribers", { name, email });
+};
+
+// item B)
+const createNewAndSendNotifications = async (
+  title: string,
+  content: string
+): Promise<void> => {
+  try {
+    await axios.put(baseUrl + "news", { title, content, date: Date.now() });
+    sendNotificationsWithPromiseAll(
+      await getSubscribers(),
+      `Nova noticia: ${title}`
+    );
+  } catch (error) {
+    console.log("ops!", error.message);
+  }
+};
+
+// item C)
+const getEveryoneNotifications = async () => {
+  const users = await getSubscribers();
+  const promisesArray: any[] = [];
+
+  for (let user of users) {
+    promisesArray.push(
+      axios.get(baseUrl + `subscribers/${user.id}/notifications/all`)
+    );
+  }
+  const res = await Promise.all(promisesArray);
+
+  res.forEach((item) => console.log(item.data));
+};
+// subscribers/:id/notifications/all
+// main
+// (async () => {
+//   sendNotificationsWithPromiseAll(
+//     await getSubscribers(),
+//     `Renan mandou um 'Olar!' no dia ${moment().format(
+//       "DD [de] MMMM [as] HH:mm"
+//     )}`
+//   );
+// })();
+// (async () => {
+//   createNewAndSendNotifications("Renan testando", "Oie");
+// })();
+(async () => {
+  getEveryoneNotifications();
+})();
