e7bde44b7f8e87596afaffd237729945e6c80d86
Renan Takeshi <rtakeshi13@gmail.com>
2020-09-01 12:14:15 -0300

    ex3 e ex4

---
+++ semana19/aula64/src/ex3.ts
@@ -0,0 +1,25 @@
+import { validateCharacter, Character } from "./ex1";
+
+export const performAttack = (attacker: Character, defender: Character) => {
+  if (!validateCharacter(attacker) || !validateCharacter(defender)) {
+    throw new Error("Invalid character");
+  }
+
+  if (attacker.strength > defender.defense) {
+    defender.life -= attacker.strength - defender.defense;
+  }
+};
+
+export const performAttackInv = (
+  attacker: Character,
+  defender: Character,
+  validator: (input: Character) => boolean
+) => {
+  if (!validator(attacker) || !validator(defender)) {
+    throw new Error("Invalid character");
+  }
+
+  if (attacker.strength > defender.defense) {
+    defender.life -= attacker.strength - defender.defense;
+  }
+};
