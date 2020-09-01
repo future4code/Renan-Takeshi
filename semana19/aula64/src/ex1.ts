45290ab535ecb8dcbe9e076ab560d246383e1ad0
Renan Takeshi <rtakeshi13@gmail.com>
2020-09-01 11:33:45 -0300

    ex1 e ex2

---
+++ semana19/aula64/src/ex1.ts
@@ -0,0 +1,23 @@
+export interface Character {
+  name: string;
+  life: number;
+  strength: number;
+  defense: number;
+}
+
+export const validateCharacter = (input: Character): boolean => {
+  if (
+    !input.name ||
+    input.life === undefined ||
+    input.strength === undefined ||
+    input.defense === undefined
+  ) {
+    return false;
+  }
+
+  if (input.life < 0 || input.strength < 0 || input.defense < 0) {
+    return false;
+  }
+
+  return true;
+};
