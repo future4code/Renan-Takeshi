1a92ea4a5913fe2b199bd6d11cfd77154d854eb2
Renan Takeshi <rtakeshi13@gmail.com>
2020-10-14 16:31:48 -0300

    findSumIndexes v2

--- logic-exercises/src/findSumIndexes.ts
+++ logic-exercises/src/findSumIndexes.ts
@@ -1,11 +1,11 @@
-export function findSumIndexes(array: number[], sum: number): number[] {
+export function findSumIndexes(numbers: number[], sum: number): number[] {
   let substractionHash = new Map();
 
-  for (let i = 0; i < array.length; i++) {
-    if (substractionHash.has(array[i])) {
-      return [substractionHash.get(array[i]), i];
+  for (let i = 0; i < numbers.length; i++) {
+    if (substractionHash.has(numbers[i])) {
+      return [substractionHash.get(numbers[i]), i];
     } else {
-      substractionHash.set(sum - array[i], i);
+      substractionHash.set(sum - numbers[i], i);
     }
   }
   return [];
